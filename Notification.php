943c30f821e6f232a9024bf3df48f77d4a52abc9
preneesh <preneesh@dreamstertech.com>
2020-05-12 14:54:18 +0530

    Post testing

--- Notification.php
+++ Notification.php
@@ -3,92 +3,73 @@ namespace preneesh\FirebaseNotifications;
 
 
 use Yii;
-use yii\helpers\ArrayHelper;
-use yii\base\BaseObject;
+use yii\base\Component;
+use yii\httpclient\Client;
+
 /**
  * @author Preneesh AV
- */
-class Notification extends BaseObject
+ **/
+
+class Notification extends Component
 {
     /**
-     * @var string the auth_key Firebase cloud messageing server key.
-     */
+     * @var string the auth_key Firebase cloud messageing Server key
+     * which can be obtained from Firebase console -> Project Overview -> Settings -> Cloud Messaging
+    * */
     public $authKey;
-
-    public $timeout = 5;
-    public $sslVerifyHost = false;
-    public $sslVerifyPeer = false;
 
     /**
      * @var string the api_url for Firebase cloude messageing.
-     */
+    **/
+
     public $apiUrl = 'https://fcm.googleapis.com/fcm/send';
 
-    public function init()
+    public function __construct($key)
     {
+        $this->authKey = $key;
         if (!$this->authKey) throw new \Exception("Empty authKey");
     }
 
     /**
      * send raw body to FCM
-     * @param array $body
+     * @param array $token ,$message
      * @return mixed
      */
-    public function send($body)
+    public function send($tokens,$message)
     {
-        $headers = [
-            "Authorization:key={$this->authKey}",
-            'Content-Type: application/json',
-            'Expect: ',
-        ];
+      $notification = [
+                    "title"             => $message['title'],
+                    "body"              => $message['body'],
+                    'sound'             => 'default',
+                  ];
 
-        $ch = curl_init($this->apiUrl);
-        curl_setopt_array($ch, [
-            CURLOPT_POST           => true,
-            CURLOPT_SSL_VERIFYHOST => $this->sslVerifyHost,
-            CURLOPT_SSL_VERIFYPEER => $this->sslVerifyPeer,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_BINARYTRANSFER => true,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HEADER         => false,
-            CURLOPT_FRESH_CONNECT  => false,
-            CURLOPT_FORBID_REUSE   => false,
-            CURLOPT_HTTPHEADER     => $headers,
-            CURLOPT_TIMEOUT        => $this->timeout,
-            CURLOPT_POSTFIELDS     => json_encode($body),
-        ]);
-        $result = curl_exec($ch);
-        if ($result === false) {
-            Yii::error('Curl failed: '.curl_error($ch).", with result=$result");
-            throw new \Exception("Could not send notification");
-        }
-        $code = (int)curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($code<200 || $code>=300) {
-            Yii::error("got unexpected response code $code with result=$result");
-            throw new \Exception("Could not send notification");
-        }
-        curl_close($ch);
-        $result = json_decode($result , true);
-        return $result;
-    }
+      $fields = array(
+                    'to' => $tokens,
+                    'notification'         => $notification,
+                    'android'         => [
+                        "notification"      => $notification,
+                    ],
+                    'apns'         => [
+                        "payload"      => [
+                            "sound"      => 'default',
+                        ],
+                    ],
+                );
 
-    /**
-     * high level method to send notification for a specific tokens (registration_ids) with FCM
-     * see https://firebase.google.com/docs/cloud-messaging/http-server-ref
-     * see https://firebase.google.com/docs/cloud-messaging/concept-options#notifications_and_data_messages
-     *
-     * @param array  $tokens the registration ids
-     * @param array  $notification can be something like {title:, body:, sound:, badge:, click_action:, }
-     * @param array  $options other FCM options https://firebase.google.com/docs/cloud-messaging/http-server-ref#downstream-http-messages-json
-     * @return mixed
-     */
-    public function sendNotification($tokens = [], $notification, $options = [])
-    {
-        $body = [
-            'registration_ids' => $tokens,
-            'notification' => $notification,
-        ];
-        $body = ArrayHelper::merge($body, $options);
-        return $this->send($body);
+        $client = new Client();
+        $response = $client->createRequest()
+                ->setMethod('POST')
+                ->setUrl($this->apiUrl)
+                ->setData( $fields)
+                ->setFormat(Client::FORMAT_JSON)
+                ->setHeaders(['content-type' => 'application/json'])
+                ->addHeaders(['Authorization' => 'key=' . $this->authKey])
+                ->send();
+
+
+        if ($response->isOk) {
+            $fcm_response = $response->getData();
+            return $fcm_response;
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # yii2-firebase-notifications
-Send firebase notification from Yii2 application (Beta version)
+Send firebase notification from Yii2 application 
 
 ==
 
@@ -29,11 +29,19 @@ Usage
 -----
 
 Once the extension is installed, simply use it in your code by  :
-
+Firebase cloud messageing Server key is required to use the extension,
+which can be obtained from Firebase console -> Project Overview -> Settings -> Cloud Messaging
 ```php
 
 use preneesh\FirebaseNotifications\Notification;
 
 $service = new Notification('YOUR_KEY');
+
+$message = array();
+$message['title']= "New Offer";
+$message['body']= "New Year offer for the all products";
+
+#unique token to be captured from android app and stored against user for sending notification to specific user
+$tokens = "d-Uh6MC1eTk:APA91bHjvSR3BDjfBqt4-ZkPbu69aije4HCzcKqvIpcBJof8Zc7z8jVeVb1WLGEjfgd6POBfW-qGUcSsESiOAOKbFnOHt_m1rk-deElMgIsojS7gpF7trgYmsLjrxICsZg7LRgPXyn6b-eqCk9nE";
 
 $service->send($tokens,$message);
